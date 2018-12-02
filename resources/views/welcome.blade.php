cb8a69bb3769cbf638c8267150ce564047d22f0f
MiltonMatias22 <nodout.develops@gmail.com>
2018-12-01 23:07:03 -0200

    Config database connection & create posts table

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - Notifications
                 </div>
 
                 <div class="links">
