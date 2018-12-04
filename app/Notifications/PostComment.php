3800a15efab8c029989da331477fa26e248e7f3d
MiltonMatias22 <nodout.develops@gmail.com>
2018-12-04 10:36:39 -0200

    Notification by queue

--- app/Notifications/PostComment.php
+++ app/Notifications/PostComment.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use App\Models\Comment;
 
-class PostComment extends Notification
+class PostComment extends Notification implements ShouldQueue
 {
     use Queueable;
 
